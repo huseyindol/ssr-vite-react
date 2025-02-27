983d6087dc410c39a6f659dfa13d1af2acf988ab
Huseyin DOL <huseyindol@gmail.com>
2025-02-27 05:51:11 +0300

    404 added redirect "/qwe" example added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import About from "./pages/about/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import We from "./pages/about/we";
 import Team from "./pages/about/team";
 import BuildInfo from "./components/BuildInfo";
+import NotFound from "./pages/NotFound";
 
 function App() {
 	console.log("App");
@@ -16,6 +17,8 @@ function App() {
 				<Route path="/about/we" element={<We />} />
 				<Route path="/about/team" element={<Team />} />
 				<Route path="/contact" element={<Contact />} />
+				<Route path="/qwe" element={<Navigate to="/" replace />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 			<BuildInfo position="bottom-right" />
 		</>
